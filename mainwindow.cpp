a8dc3055ee7381ec3c49ac2075de8054d972afe1
Ramir Sultanov <ramirrsultanov@stud.kpfu.ru>
2021-02-12 15:55:27 +0300

    commit 2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,80 +16,78 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_make_pizza_clicked()
 {
-    std::string text = "Пожалуйста выберите все компоненты";
-    if (
-        (ui->olives_no->isChecked() || ui->olives_yes->isChecked())
-        && (ui->pepperoni_no->isChecked() || ui->pepperoni_yes->isChecked())
-        && (ui->double_no->isChecked() || ui->double_yes->isChecked())
-        && (ui->pineapples_no->isChecked() || ui->pineapples_yes->isChecked())
-    )
+  std::string text = "Пожалуйста выберите все компоненты";
+  if ((ui->olives_no->isChecked() || ui->olives_yes->isChecked()) &&
+      (ui->pepperoni_no->isChecked() || ui->pepperoni_yes->isChecked()) &&
+      (ui->double_no->isChecked() || ui->double_yes->isChecked()) &&
+      (ui->pineapples_no->isChecked() || ui->pineapples_yes->isChecked()))
+  {
+    PizzaBuilder pizza_builder;
+
+    if (ui->thin->isChecked())
     {
-        PizzaBuilder pizza_builder;
-
-        if (ui->thin->isChecked())
-        {
-            pizza_builder.setDough(DOUGH_THIN);
-        }
-        else
-        {
-            pizza_builder.setDough(DOUGH_TRADITIONAL);
-        }
-
-        if (ui->pepperoni->isChecked())
-        {
-            pizza_builder.setType(TYPE_PEPPERONI);
-        }
-        else if (ui->margaret->isChecked())
-        {
-            pizza_builder.setType(TYPE_MARGARET);
-        }
-        else if (ui->four_cheeses->isChecked())
-        {
-            pizza_builder.setType(TYPE_FOUR_CHEESES);
-        }
-        else
-        {
-            pizza_builder.setType(TYPE_MARINE);
-        }
-
-        if (ui->olives_no->isChecked())
-        {
-            pizza_builder.setOlives(NO);
-        }
-        else
-        {
-            pizza_builder.setOlives(YES);
-        }
-
-        if (ui->pepperoni_no->isChecked())
-        {
-            pizza_builder.setPepperoni(NO);
-        }
-        else
-        {
-            pizza_builder.setPepperoni(YES);
-        }
-
-        if (ui->double_no->isChecked())
-        {
-            pizza_builder.setDoubleCheese(NO);
-        }
-        else
-        {
-            pizza_builder.setDoubleCheese(YES);
-        }
-
-        if (ui->pineapples_no->isChecked())
-        {
-            pizza_builder.setPineapples(NO);
-        }
-        else
-        {
-            pizza_builder.setPineapples(YES);
-        }
-
-        Pizza pizza = pizza_builder.build();
-        text = pizza.to_string();
+      pizza_builder.setDough(DOUGH_THIN);
     }
-    ui->textEdit->setText(QString::fromStdString(text));
+    else
+    {
+      pizza_builder.setDough(DOUGH_TRADITIONAL);
+    }
+
+    if (ui->pepperoni->isChecked())
+    {
+      pizza_builder.setType(TYPE_PEPPERONI);
+    }
+    else if (ui->margaret->isChecked())
+    {
+      pizza_builder.setType(TYPE_MARGARET);
+    }
+    else if (ui->four_cheeses->isChecked())
+    {
+      pizza_builder.setType(TYPE_FOUR_CHEESES);
+    }
+    else
+    {
+      pizza_builder.setType(TYPE_MARINE);
+    }
+
+    if (ui->olives_no->isChecked())
+    {
+      pizza_builder.setOlives(NO);
+    }
+    else
+    {
+      pizza_builder.setOlives(YES);
+    }
+
+    if (ui->pepperoni_no->isChecked())
+    {
+      pizza_builder.setPepperoni(NO);
+    }
+    else
+    {
+      pizza_builder.setPepperoni(YES);
+    }
+
+    if (ui->double_no->isChecked())
+    {
+      pizza_builder.setDoubleCheese(NO);
+    }
+    else
+    {
+      pizza_builder.setDoubleCheese(YES);
+    }
+
+    if (ui->pineapples_no->isChecked())
+    {
+      pizza_builder.setPineapples(NO);
+    }
+    else
+    {
+      pizza_builder.setPineapples(YES);
+    }
+
+    Pizza pizza = pizza_builder.build();
+    text = pizza.to_string();
+  }
+  ui->textEdit->setText(QString::fromStdString(text));
 }
--- pizza.h
+++ pizza.h
@@ -3,40 +3,55 @@
 
 #include <iostream>
 
-enum Dough { DOUGH_THIN, DOUGH_TRADITIONAL };
-enum Type { TYPE_PEPPERONI, TYPE_MARGARET, TYPE_FOUR_CHEESES, TYPE_MARINE };
-enum Bool { NO, YES };
+enum Dough
+{
+  DOUGH_THIN,
+  DOUGH_TRADITIONAL
+};
+enum Type
+{
+  TYPE_PEPPERONI,
+  TYPE_MARGARET,
+  TYPE_FOUR_CHEESES,
+  TYPE_MARINE
+};
+enum Bool
+{
+  NO,
+  YES
+};
 
 class Pizza
 {
-    Dough dough_;
-    Type type_;
-    Bool olives_;
-    Bool pepperoni_;
-    Bool double_cheese_;
-    Bool pineapples_;
+  Dough dough_;
+  Type type_;
+  Bool olives_;
+  Bool pepperoni_;
+  Bool double_cheese_;
+  Bool pineapples_;
+
 public:
-    Pizza(Dough dough, Type type, Bool olives, Bool pepperoni, Bool double_cheese, Bool pineapples);
-    std::string to_string();
+  Pizza(Dough dough, Type type, Bool olives, Bool pepperoni, Bool double_cheese, Bool pineapples);
+  std::string to_string();
 };
 
 class PizzaBuilder
 {
-    Dough dough_;
-    Type type_;
-    Bool olives_;
-    Bool pepperoni_;
-    Bool double_cheese_;
-    Bool pineapples_;
+  Dough dough_;
+  Type type_;
+  Bool olives_;
+  Bool pepperoni_;
+  Bool double_cheese_;
+  Bool pineapples_;
+
 public:
-    void setDough(Dough dough);
-    void setType(Type type);
-    void setOlives(Bool olives);
-    void setPepperoni(Bool pepperoni);
-    void setDoubleCheese(Bool double_cheese);
-    void setPineapples(Bool pineapples);
-    Pizza build();
+  void setDough(Dough dough);
+  void setType(Type type);
+  void setOlives(Bool olives);
+  void setPepperoni(Bool pepperoni);
+  void setDoubleCheese(Bool double_cheese);
+  void setPineapples(Bool pineapples);
+  Pizza build();
 };
 
-
-#endif // PIZZA_H
+#endif  // PIZZA_H
